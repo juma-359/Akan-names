3e04e0300fe4a680e9dceb7a5fcbadb2a486b1f7
juma-359 <joeljuma08@gmail.com>
2020-02-16 17:38:43 +0300

    changes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
-var akan = document.getElementById('akan');
-var akanTitle = document.getElementsByClassName('akan-title');
-var form = document.getElementsByTagName('') 
+// var akan = document.getElementById('akan');
+// var akanTitle = document.getElementsByClassName('akan-title');
+// var form = document.getElementsByTagName('') 
+
+// function click(male,female){
+//     if(male = "male"){
+        
+//     }
+// }
+
+// var contactForm = document.getElementById('contact-form');
+// // console.log(contactForm);
+
