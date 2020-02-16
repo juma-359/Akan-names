6f22e8a99ccd8f0589b33191993cc135eedc0150
juma-359 <joeljuma08@gmail.com>
2020-02-16 18:33:33 +0300

    changes in script.js

--- js/script.js
+++ js/script.js
@@ -29,4 +29,4 @@ function getGender(){
 }
 var maleNames =["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-var daysOfTheWeek = [];
+var daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
