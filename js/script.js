2d7e74c0c43f97af1332af865f77efae27af9cc0
juma-359 <joeljuma08@gmail.com>
2020-02-16 18:28:00 +0300

    script.js

--- js/script.js
+++ js/script.js
@@ -11,3 +11,22 @@
 // var contactForm = document.getElementById('contact-form');
 // // console.log(contactForm);
 
+// var contactForm = alert ("enter your date of birth");
+
+// if(contactForm == nul) {
+//     alert("min=1920/1/1")
+// } else {
+//     alert("sucessful")
+// }
+
+function getGender(){
+    var gender =document.getElementsByName("gender");
+    for(i=0; i< gender.length; i++){
+        if(gender[i].checked){
+            return(gender[i].value)
+        }
+    }
+}
+var maleNames =["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+var daysOfTheWeek = [];
